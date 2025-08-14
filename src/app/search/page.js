5b9f7764544b91b3f442a9e3c02a9ed7d0e8ca38
Widido1 <you@example.com>
2025-08-14 16:38:29 -0300

    try fix 1 for building

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -4,15 +4,19 @@ import SearchResults from "../components/searchresults"
 import { prisma } from "../libs/prisma";
 
 
-async function loadProducts(){
-    const allProducts = await prisma.product.findMany();
-    return allProducts;
+async function loadProducts() {
+  try {
+    return await prisma.product.findMany();
+  } catch (error) {
+    console.error("Error loading products:", error);
+    return []; // Devuelve un array vacío como fallback
+  }
 }
 
 export default async function SearchPage({params}){
     //const products = await loadProducts();
     const {value} = await params;
-    const products = loadProducts();
+    const products = await loadProducts();
     //const products = [
     //    {id: 1, name: "Budweiser", marca: "Budweiser", img: "", description: "Budweiser lager de litro", precio: 100, estilo: "Lager", contenido: "1000"},
     //];
