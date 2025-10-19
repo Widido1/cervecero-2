c5662bf7dc6a74001a6b53a03a7241c1101bc33e
Widido1 <you@example.com>
2025-10-19 19:50:10 -0300

    Email SMTP

---
+++ src/app/libs/email-config.js
@@ -0,0 +1,13 @@
+import nodemailer from 'nodemailer';
+
+export const createTransporter = () => {
+  return nodemailer.createTransport({
+    host: process.env.SMTP_HOST || 'smtp.hostinger.com',
+    port: parseInt(process.env.SMTP_PORT) || 465,
+    secure: true,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+};
