1d14067a1e0f6cf37d1490fe7e230b80c4f2253f
Widido1 <you@example.com>
2025-10-31 18:35:21 -0300

    Return to Resend

--- src/app/actions/customerEmailSC.js
+++ src/app/actions/customerEmailSC.js
@@ -1,4 +1,22 @@
 "use server"
+import { Resend } from "resend"
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const customerEmailSC = async (data) => {
+    const email = data.email;
+    const message = data.text;
+
+    await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: email,
+        subject: "SOLICITUD DE COMPRA REALIZADA ",
+        text: message,
+        replyTo: email,
+    });
+}
+
+/*
 import { createTransporter } from '@/app/libs/email-config';
 
 export const customerEmailSC = async (emailData) => {
@@ -27,3 +45,4 @@ export const customerEmailSC = async (emailData) => {
     return { success: false, error: error.message };
   }
 };
+*/
