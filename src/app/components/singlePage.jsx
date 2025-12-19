154231b21d954edec66d95a7cc432804e8c39f44
Widido1 <you@example.com>
2025-12-18 22:13:02 -0300

    Whatsapp Button 1

--- src/app/components/singlePage.jsx
+++ src/app/components/singlePage.jsx
@@ -5,6 +5,7 @@ import Navbar from "./navbar";
 import Pie from "./pie";
 import Slider from "./slider";
 import Cart from "./cart";
+import WSP from "./wsp";
 
 export default function SinglePage(props) {
     const product = props.product; //aqui van los items que se pasan al slider, por ahora un array vacio
@@ -14,6 +15,7 @@ export default function SinglePage(props) {
         <div className="min-w-[400px]">
             <Navbar display={displayCart} setDisplay={setDisplayCart}/>
             <div className="grid h-[120px]"></div> {/* Espacio para el navbar */}
+            <WSP/>
             <div className="grid place-content-center place-items-center gap-8 py-8">
                 <div className="grid">
                     <SingleCard id={product.id} name={product.name} img={product.imageUrl} des={product.description} price={product.price} type={product.type}/>
