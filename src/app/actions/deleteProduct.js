7bf0a90075e0d77a15a5cd07ffa86f86e6e1f6b9
Widido1 <sdg-1995@protonmail.com>
2026-02-25 21:41:33 -0300

    Admin 03

--- src/app/actions/deleteProduct.js
+++ src/app/actions/deleteProduct.js
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { prisma } from "../libs/prisma";
+import { revalidatePath } from "next/cache";
 
 export default async function DeleteProduct(id){
     await prisma.product.delete({
